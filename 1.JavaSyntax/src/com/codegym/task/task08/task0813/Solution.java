9fd02d8425e40e54d71bec83f03bd6aec1c9da2c
aggreyah <aggreyah@gmail.com>
2021-05-07 11:30:31 +0300

    level 8 task 13 done.

---
+++ 1.JavaSyntax/src/com/codegym/task/task08/task0813/Solution.java
@@ -0,0 +1,43 @@
+package com.codegym.task.task08.task0813;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 words that start with the letter "L"
+
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //write your code here
+        HashSet<String> set = new HashSet<>();
+        set.add("Land");
+        set.add("Lamp");
+        set.add("Last");
+        set.add("Lest");
+        set.add("Least");
+        set.add("Limp");
+        set.add("Lift");
+        set.add("Left");
+        set.add("Love");
+        set.add("Lovely");
+        set.add("Ladder");
+        set.add("Labor");
+        set.add("Lack");
+        set.add("Laugh");
+        set.add("Latter");
+        set.add("Lake");
+        set.add("Label");
+        set.add("Landlord");
+        set.add("Landing");
+        set.add("Lane");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
