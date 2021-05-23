eae0dd0cb7a76956b11e5c72e7b5f9de514f403d
aggreyah <aggreyah@gmail.com>
2021-05-23 13:31:37 +0300

    level 10 task 13 done.

--- 1.JavaSyntax/src/com/codegym/task/task10/task1013/Solution.java
+++ 1.JavaSyntax/src/com/codegym/task/task10/task1013/Solution.java
@@ -11,5 +11,71 @@ public class Solution {
 
     public static class Human {
         // Write your variables and constructors here
+        private String firstName;
+        private String secondName;
+        private String yearOfBirth;
+        private String countryOfBirth;
+        private String eyeColor;
+        private boolean male;
+
+        public Human(){}
+
+        public Human(String name){this.firstName = name;}
+
+        public Human(String first, String second){
+            this.firstName = first;
+            this.secondName = second;
+        }
+
+        public Human(String first, String second, String year){
+            this.firstName = first;
+            this.secondName = second;
+            this.yearOfBirth = year;
+        }
+
+        public Human(boolean male, String first, String second){
+            this.male = male;
+            this.firstName = first;
+            this.secondName = second;
+        }
+
+        public Human(boolean male, String first, String second, String country){
+            this.male = male;
+            this.firstName = first;
+            this.secondName = second;
+            this.countryOfBirth = country;
+        }
+
+        public Human(boolean male, String first, String second, String country, String eyes){
+            this.male = male;
+            this.firstName = first;
+            this.secondName = second;
+            this.countryOfBirth = country;
+            this.eyeColor = eyes;
+        }
+
+        public Human(String first, String second, String year, String country){
+            this.firstName = first;
+            this.secondName = second;
+            this.yearOfBirth = year;
+            this.countryOfBirth = country;
+        }
+
+        public Human(String first, String second, String year, String country, String eyes){
+            this.firstName = first;
+            this.secondName = second;
+            this.yearOfBirth = year;
+            this.countryOfBirth = country;
+            this.eyeColor = eyes;
+        }
+
+        public Human(String first, String second, String year, String country, String eyes, boolean male){
+            this.firstName = first;
+            this.secondName = second;
+            this.yearOfBirth = year;
+            this.countryOfBirth = country;
+            this.eyeColor = eyes;
+            this.male = male;
+        }
     }
 }
