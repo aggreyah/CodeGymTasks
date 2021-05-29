4ea449c1d7434452ed02eeb381ce81c10dbf96a9
aggreyah <aggreyah@gmail.com>
2021-05-29 17:37:02 +0300

    completed up to task 1210.

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1207/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task12.task1207;
+
+/* 
+int and Integer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+        print(Integer.valueOf(2));
+    }
+
+    //write your code here
+    public static void print(int i){System.out.println("first print.");}
+    public static void print(Integer i){System.out.println("good print");}
+}
---
+++ 2.JavaCore/src/com/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,19 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static void print(){}
+    public static void print(boolean i){}
+    public static void print(String s) {}
+    public static void print(int i){}
+    public static void print(char c){}
+}
