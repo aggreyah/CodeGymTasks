8aad2bbf5b4b0bc5c15c80db83359d99f69118c2
aggreyah <aggreyah@gmail.com>
2021-07-02 21:29:50 +0300

    finished task 1503.

--- 2.JavaCore/src/com/codegym/task/task14/task1421/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task14/task1421/Solution.java
@@ -7,5 +7,9 @@ Singleton
 
 public class Solution {
     public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+        Singleton s1 = Singleton.getInstance();
+        System.out.println(s);
+        System.out.println(s1);
     }
 }
