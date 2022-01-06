19316601e5c506b0b922d363ec158c32cd9b2eee
aggreyah <aggreyah@gmail.com>
2022-01-06 17:39:23 +0300

    task 1518 completed.

--- 2.JavaCore/src/com/codegym/task/task15/task1518/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task15/task1518/Solution.java
@@ -8,6 +8,17 @@ Static modifiers and kittens
 public class Solution {
     public static Cat cat;
 
+    static {
+        Cat c = new Cat();
+        c.name = "Schroedinger";
+        cat =c;
+        System.out.println(cat.name);
+    }
+
+    public static class Cat{
+        public String name;
+    }
+
     public static void main(String[] args) {
 
     }
