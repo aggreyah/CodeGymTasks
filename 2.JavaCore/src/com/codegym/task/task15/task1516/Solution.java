037a1898535cd8599dbf5027c3f182f25e006782
aggreyah <aggreyah@gmail.com>
2022-01-06 17:14:19 +0300

    task 1516 done and task 1517 added.

--- 2.JavaCore/src/com/codegym/task/task15/task1516/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task15/task1516/Solution.java
@@ -6,6 +6,23 @@ Default values
 */
 
 public class Solution {
+    int intVar;
+    double doubleVar;
+    Double DoubleVar;
+    boolean booleanVar;
+    Object ObjectVar;
+    Exception ExceptionVar;
+    String StringVar;
+
+
     public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.intVar);
+        System.out.println(s.doubleVar);
+        System.out.println(s.DoubleVar);
+        System.out.println(s.booleanVar);
+        System.out.println(s.ObjectVar);
+        System.out.println(s.ExceptionVar);
+        System.out.println(s.StringVar);
     }
 }
