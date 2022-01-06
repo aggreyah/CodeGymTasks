89ef1fe99537afd9913b2d7646dbb0323876a167
aggreyah <aggreyah@gmail.com>
2022-01-06 18:28:00 +0300

    task 1519 added.

---
+++ 2.JavaCore/src/com/codegym/task/task15/task1519/Solution.java
@@ -0,0 +1,32 @@
+package com.codegym.task.task15.task1519;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+/* 
+Different methods for different types
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //write your code here
+    }
+
+    public static void print(Double value) {
+        System.out.println("This is a Double. Value: " + value);
+    }
+
+    public static void print(String value) {
+        System.out.println("This is a String. Value: " + value);
+    }
+
+    public static void print(short value) {
+        System.out.println("This is a short. Value: " + value);
+    }
+
+    public static void print(Integer value) {
+        System.out.println("This is an Integer. Value: " + value);
+    }
+}
