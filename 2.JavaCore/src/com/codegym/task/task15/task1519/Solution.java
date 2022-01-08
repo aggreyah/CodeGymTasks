324003e9f5f78fbc6944095edfd9e3be3b950239
aggreyah <aggreyah@gmail.com>
2022-01-08 14:09:48 +0300

    finished task 1519

--- 2.JavaCore/src/com/codegym/task/task15/task1519/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task15/task1519/Solution.java
@@ -12,6 +12,60 @@ Different methods for different types
 public class Solution {
     public static void main(String[] args) throws IOException {
         //write your code here
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String userInput = "";
+
+        while (!userInput.equals("exit")){
+            System.out.print("Enter a string value or exit to quit:");
+            userInput = bufferedReader.readLine();
+            if (userInput.equals("exit"))
+                break;
+            if (isDouble(userInput))
+                print(Double.parseDouble(userInput));
+            else if (isShort(userInput))
+                print(Short.parseShort(userInput));
+            else if (isInteger(userInput))
+                print(Integer.parseInt(userInput));
+            else
+                print(userInput);
+        }
+    }
+
+    public static boolean isDouble(String s){
+        boolean isADouble = true;
+        try {
+            Double.parseDouble(s);
+            if (!s.contains("."))
+                isADouble = false;
+        } catch (NumberFormatException e) {
+            isADouble = false;
+        }
+        return isADouble;
+    }
+
+    public static boolean isShort(String s){
+        boolean isAShort = true;
+        try {
+            Integer myInt = Integer.parseInt(s);
+            if (!(myInt > 0 && myInt < 128)){
+                isAShort = false;
+            }
+        } catch (NumberFormatException e) {
+            isAShort = false;
+        }
+        return isAShort;
+    }
+
+    public static boolean isInteger(String s){
+        boolean isAnInteger = true;
+        try {
+            Integer myInt = Integer.parseInt(s);
+            if (!(myInt <= 0 || myInt >= 128))
+                isAnInteger = false;
+        } catch (NumberFormatException e) {
+            isAnInteger = false;
+        }
+        return isAnInteger;
     }
 
     public static void print(Double value) {
