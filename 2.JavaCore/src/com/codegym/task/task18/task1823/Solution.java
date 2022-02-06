c0761a7c6102bc8c931b6536262c897bbb02d3da
aggreyah <aggreyah@gmail.com>
2022-02-07 01:56:26 +0300

    added & completed task 1823.

--- 2.JavaCore/src/com/codegym/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task18/task1823/Solution.java
@@ -1,8 +1,6 @@
 package com.codegym.task.task18.task1823;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,14 +17,49 @@ public class Solution {
         String currentFile = bufferedReader.readLine();
         while (!currentFile.equals("exit")){
             ReadThread currentThread = new ReadThread(currentFile);
+            currentThread.start();
+            currentFile = bufferedReader.readLine();
         }
-
     }
 
     public static class ReadThread extends Thread {
+        String fileName;
         public ReadThread(String fileName) {
             // Implement constructor body
+            this.fileName = fileName;
         }
         // Implement file reading here
+        @Override
+        public void run() {
+            try {
+                FileInputStream fileInputStream = new FileInputStream(fileName);
+                byte[] buffer = new byte[fileInputStream.available()];
+                fileInputStream.read(buffer);
+                fileInputStream.close();
+
+                Map<Byte, Integer> threadMap = new HashMap<>();
+                for(byte item: buffer){
+                    if (threadMap.containsKey(item))
+                        threadMap.replace(item, threadMap.get(item) + 1);
+                    else
+                        threadMap.put(item, 1);
+                }
+                int max = 0;
+                for (Map.Entry<Byte, Integer> entry:threadMap.entrySet())
+                    if (entry.getValue() > max)
+                        max = entry.getValue();
+
+                int mostFrequentKey = 0;
+                for (Map.Entry<Byte, Integer> entry: threadMap.entrySet())
+                    if (entry.getValue() == max)
+                        mostFrequentKey = entry.getKey();
+
+                synchronized (resultMap){
+                    resultMap.put(this.fileName, mostFrequentKey);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
