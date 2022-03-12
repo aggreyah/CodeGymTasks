b14393928a016acbc3d6baa6bc687b32669d8fff
aggreyah <aggreyah@gmail.com>
2022-03-12 14:31:59 +0300

    added task 2022. It is not clear to me how it goes in this task. May it will make sense later.

---
+++ 2.JavaCore/src/com/codegym/task/task20/task2022/Solution.java
@@ -0,0 +1,43 @@
+package com.codegym.task.task20.task2022;
+
+import java.io.*;
+
+/* 
+Overriding serialization in a thread
+
+*/
+
+public class Solution implements Serializable, AutoCloseable {
+    private final String fileName;
+    transient private FileOutputStream stream;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
+        this.stream = new FileOutputStream(this.fileName);
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        this.stream = new FileOutputStream(fileName,true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
