195c415ca8ad50e24b6626c01207d7e6313d74a9
aggreyah <aggreyah@gmail.com>
2022-03-19 18:27:05 +0300

    added & finished task 2108.

---
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2108/Solution.java
@@ -0,0 +1,58 @@
+package com.codegym.task.task21.task2108;
+
+/* 
+Cloning plants
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Tree tree = new Tree("willow", new String[]{"s1", "s2", "s3", "s4"});
+        Tree clone = null;
+        try {
+            clone = (Tree) tree.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(tree);
+        System.out.println(clone);
+
+        System.out.println(tree.branches);
+        System.out.println(clone.branches);
+    }
+
+    public static class Plant{
+        private String name;
+
+        public Plant(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    public static class Tree extends Plant implements Cloneable{
+        private String[] branches;
+
+        public Tree(String name, String[] branches) {
+            super(name);
+            this.branches = branches;
+        }
+
+        public String[] getBranches() {
+            return branches;
+        }
+
+        @Override
+        protected Object clone() throws CloneNotSupportedException {
+            Tree tree = new Tree(this.getName(), new String[this.getBranches().length]);
+            for (int i = 0; i < tree.getBranches().length; i++){
+                tree.getBranches()[i] = this.getBranches()[i];
+            }
+            return tree;
+        }
+    }
+}
