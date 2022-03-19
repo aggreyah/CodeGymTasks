29927a1665b2ef841aab46c7ab9c9252f35e3626
aggreyah <aggreyah@gmail.com>
2022-03-19 23:47:50 +0300

    completed hippodrome part6.

--- 3.JavaMultithreading/src/com/codegym/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2113/Hippodrome.java
@@ -1,16 +1,27 @@
 package com.codegym.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
     private List<Horse> horses;
+    static Hippodrome game;
 
     public Hippodrome(List<Horse> listOfHorses){
         this.horses = listOfHorses;
     }
     
     public static void main(String[] args) {
+        Horse horse1 = new Horse("Horse1", 3, 0);
+        Horse horse2 = new Horse("Horse2", 3, 0);
+        Horse horse3 = new Horse("Horse3", 3, 0);
 
+        List<Horse> myHorses = new ArrayList<>();
+        myHorses.add(horse1);
+        myHorses.add(horse2);
+        myHorses.add(horse3);
+
+        game = new Hippodrome(myHorses);
     }
 
     public List<Horse> getHorses() {
