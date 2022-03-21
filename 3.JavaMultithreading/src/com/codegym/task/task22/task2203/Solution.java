cbd98bcf69fb75a780ef95650592fe78a1e659be
aggreyah <aggreyah@gmail.com>
2022-03-21 23:47:48 +0300

    added & completed task 2204.

--- 3.JavaMultithreading/src/com/codegym/task/task22/task2203/Solution.java
+++ 3.JavaMultithreading/src/com/codegym/task/task22/task2203/Solution.java
@@ -9,9 +9,9 @@ public class Solution {
     public static String getPartOfString(String string) throws StringTooShortException {
         String result;
         try {
-            int start = string.indexOf("\t");
-            int stop = string.indexOf("\t", start + 1);
-            result = string.substring(start + 1, stop);
+            int start = string.indexOf("\t") + 1;
+            int stop = string.indexOf("\t", start);
+            result = string.substring(start, stop);
         } catch (Exception e) {
             throw new StringTooShortException();
         }
